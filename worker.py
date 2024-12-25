02f9d6b96b192abeab77e6e3316839e0677fc719
Thai Nguyen <thainguyen20594@gmail.com>
2024-12-25 13:53:57 -0500

    Initital env setup and updating worker.py

--- worker.py
+++ worker.py
@@ -12,6 +12,9 @@ from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
 from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
 from ibm_watson_machine_learning.foundation_models import Model
 
+from langchain import PromptTemplate
+#from langchain.chains import LLMChain, SimpleSequentialChain
+
 # Check for GPU availability and set the appropriate device for computation.
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -21,59 +24,73 @@ chat_history = []
 llm_hub = None
 embeddings = None
 
-Watsonx_API = "Your WatsonX API"
-Project_id= "Your Project ID"
-
 # Function to initialize the language model and its embeddings
 def init_llm():
     global llm_hub, embeddings
-    
+
+    my_credentials = {
+"url"    : "https://us-south.ml.cloud.ibm.com"
+}
+
+
     params = {
-        GenParams.MAX_NEW_TOKENS: 250, # The maximum number of tokens that the model can generate in a single run.
-        GenParams.MIN_NEW_TOKENS: 1,   # The minimum number of tokens that the model should generate in a single run.
-        GenParams.DECODING_METHOD: DecodingMethods.SAMPLE, # The method used by the model for decoding/generating new tokens. In this case, it uses the sampling method.
-        GenParams.TEMPERATURE: 0.1,   # A parameter that controls the randomness of the token generation. A lower value makes the generation more deterministic, while a higher value introduces more randomness.
-        GenParams.TOP_K: 50,          # The top K parameter restricts the token generation to the K most likely tokens at each step, which can help to focus the generation and avoid irrelevant tokens.
-        GenParams.TOP_P: 1            # The top P parameter, also known as nucleus sampling, restricts the token generation to a subset of tokens that have a cumulative probability of at most P, helping to balance between diversity and quality of the generated text.
-    }
-    
-    credentials = {
-        'url': "https://us-south.ml.cloud.ibm.com",
-        'apikey' : Watsonx_API
-    }
-    
+            GenParams.MAX_NEW_TOKENS: 256, # The maximum number of tokens that the model can generate in a single run.
+            GenParams.TEMPERATURE: 0.1,   # A parameter that controls the randomness of the token generation. A lower value makes the generation more deterministic, while a higher value introduces more randomness.
+        }
+
+
     LLAMA2_model = Model(
-        model_id= 'meta-llama/llama-2-70b-chat',
-        credentials=credentials,
-        params=params,
-        project_id=Project_id)
+            model_id= 'meta-llama/llama-3-8b-instruct', 
+            credentials=my_credentials,
+            params=params,
+            project_id="skills-network"
+            )
+
+
 
     llm_hub = WatsonxLLM(model=LLAMA2_model)
 
+    ### --> if you are using huggingFace API:
+    # Set up the environment variable for HuggingFace and initialize the desired model, and load the model into the HuggingFaceHub
+    # dont forget to remove llm_hub for watsonX
+
+    # os.environ["HUGGINGFACEHUB_API_TOKEN"] = "YOUR API KEY"
+    # model_id = "tiiuae/falcon-7b-instruct"
+    #llm_hub = HuggingFaceHub(repo_id=model_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 600, "max_length": 600})
+
     #Initialize embeddings using a pre-trained model to represent the text data.
-    embeddings =  # create object of Hugging Face Instruct Embeddings with (model_name,  model_kwargs={"device": DEVICE} )
+    embeddings = HuggingFaceInstructEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={"device": DEVICE}
+    )
+
 
 # Function to process a PDF document
 def process_document(document_path):
     global conversation_retrieval_chain
+
     # Load the document
-    loader =   # ---> use PyPDFLoader and document_path from the function input parameter <---
-    
+    loader = PyPDFLoader(document_path)
     documents = loader.load()
-    # Split the document into chunks, set chunk_size=1024, and chunk_overlap=64. assign it to variable text_splitter
-    text_splitter = # ---> use Recursive Character TextSplitter and specify the input parameters <---
-    
+
+    # Split the document into chunks
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=64)
     texts = text_splitter.split_documents(documents)
-    
+
     # Create an embeddings database using Chroma from the split text chunks.
     db = Chroma.from_documents(texts, embedding=embeddings)
-    
-    # Build the QA chain, which utilizes the LLM and retriever for answering questions.
+
+
+    # --> Build the QA chain, which utilizes the LLM and retriever for answering questions. 
+    # By default, the vectorstore retriever uses similarity search. 
+    # If the underlying vectorstore support maximum marginal relevance search, you can specify that as the search type (search_type="mmr").
+    # You can also specify search kwargs like k to use when doing retrieval. k represent how many search results send to llm
     conversation_retrieval_chain = RetrievalQA.from_chain_type(
         llm=llm_hub,
         chain_type="stuff",
-        retriever= db.as_retriever(search_type="mmr", search_kwargs={'k': 6, 'lambda_mult': 0.25}),
-        return_source_documents=False
+        retriever=db.as_retriever(search_type="mmr", search_kwargs={'k': 6, 'lambda_mult': 0.25}),
+        return_source_documents=False,
+        input_key = "question"
+     #   chain_type_kwargs={"prompt": prompt} # if you are using prompt template, you need to uncomment this part
     )
 
 
@@ -81,18 +98,16 @@ def process_document(document_path):
 def process_prompt(prompt):
     global conversation_retrieval_chain
     global chat_history
-    # Pass the prompt and the chat history to the conversation_retrieval_chain object
+
+    # Query the model
     output = conversation_retrieval_chain({"question": prompt, "chat_history": chat_history})
-    
-    answer =  output["result"]
-    
+    answer = output["result"]
+
     # Update the chat history
-    # TODO: Append the prompt and the bot's response to the chat history using chat_history.append and pass `prompt` `answer` as arguments
-    # --> write your code here <--	
-    
-    # Return the model's response
-    return result['answer']
-    
+    chat_history.append((prompt, answer))
+
+    # Return the model\'s response
+    return answer
 
 # Initialize the language model
 init_llm()
